ba3df82a5ad0dc833e23d0b65797d8827a8b74e6
Adam Ali <adamwaliditpro@gmail.com>
2026-04-04 10:50:54 +0200

    feat: add crud to angular, book  features with add/edit functionality and UI updates

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./home/home').then(m => m.Home), canActivate: [authGuard] },
+  { path: 'books/add', loadComponent: () => import('./book-form/book-form').then(m => m.BookForm), canActivate: [authGuard] },
+  { path: 'books/edit/:id', loadComponent: () => import('./book-form/book-form').then(m => m.BookForm), canActivate: [authGuard] },
   { path: 'login', loadComponent: () => import('./login/login').then(m => m.Login) },
   { path: 'register', loadComponent: () => import('./register/register').then(m => m.Register) },
 ];
